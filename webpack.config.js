65e9da90fec39aef5366bc11f74401253b025cac
Kristian Rekstad <krirek@msn.com>
2020-08-09 02:25:40 +0200

    build: Increase image quality

--- webpack.config.js
+++ webpack.config.js
@@ -63,14 +63,14 @@ module.exports = {
             options: {
               mozjpeg: {
                 progressive: true,
-                quality: 65,
+                quality: 80,
               },
               // optipng.enabled: false will disable optipng
               optipng: {
                 enabled: false,
               },
               pngquant: {
-                quality: [0.65, 0.9],
+                quality: [0.7, 1],
                 speed: 4,
               },
               gifsicle: {
@@ -78,7 +78,7 @@ module.exports = {
               },
               // the webp option will enable WEBP
               webp: {
-                quality: 75,
+                quality: 80,
               },
             },
           },
