34bfa402ff11baf547996cde9570459248b6e122
Kristian Rekstad <kre@capraconsulting.no>
2025-07-26 02:15:25 +0200

    Disable matomo

--- src/_data/matomo.js
+++ src/_data/matomo.js
@@ -1,5 +1,5 @@
 module.exports = {
-  enabled: true && process.env.ELEVENTY_ENV === 'production',
+  enabled: false && process.env.ELEVENTY_ENV === 'production',
   host: 'stats.krex.no',
   siteId: '1',
 };
